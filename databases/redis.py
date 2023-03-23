cd425779e32814803f3b253384309a9afb44e40e
Juxan <julyfortune101@gmail.com>
2023-03-23 04:33:21 +0300

    redis metric cache tests + improval, test_api.py crutch for tests proper scoping

--- databases/redis.py
+++ databases/redis.py
@@ -1,12 +1,13 @@
-from redis.client import Redis
-
+"""Cache related - pooling, repository."""
 from fastapi import Depends
+from redis.client import Redis
 from redis.connection import ConnectionPool
 
 from utils import get_redis_pool
 
 
 def redis_pool_acquer(pool: ConnectionPool = Depends(get_redis_pool)):
+    """Goes as context."""
     client = Redis(connection_pool=pool)
     try:
         yield RedisRepo(client)
@@ -15,13 +16,18 @@ def redis_pool_acquer(pool: ConnectionPool = Depends(get_redis_pool)):
 
 
 class RedisRepo:
+    """Redis client and database abs."""
 
     def __init__(self, conn: Redis):
+        """One per dependant."""
         self._conn = conn
 
     def check_cache(self, metric: str) -> int:
+        """Get not expired values."""
         value = self._conn.get(metric)
-        return int(value.decode())
+        if value:
+            return int(value.decode())
 
     def set_cache(self, metric: str, ttl: int, value: int):
+        """SET val EX tm wrapper."""
         self._conn.setex(metric, ttl, value)
---
+++ tests/test_redis.py
@@ -0,0 +1,48 @@
+"""Cache storage checks."""
+import time as tm
+
+import pytest
+from redis.client import Redis
+
+from databases.redis import RedisRepo
+
+
+@pytest.fixture
+def redis_client():
+    """Test connection provider."""
+    redis = Redis()
+    try:
+        yield RedisRepo(redis)
+    finally:
+        redis.close()
+
+
+class TestRedis:
+    """Redis operations."""
+
+    METRIC_NAME = 'test'
+    TEST_VAL = 7
+    EXPIRATION_TIME_SEC = 2
+
+    def test_write(self, redis_client):
+        """Store some `metric`."""
+        setex = redis_client.set_cache(
+            self.METRIC_NAME, self.EXPIRATION_TIME_SEC,
+            self.TEST_VAL)
+        assert setex is None
+
+    def test_read(self, redis_client):
+        """Easy reading."""
+        readex = redis_client.check_cache(self.METRIC_NAME)
+        assert readex == self.TEST_VAL
+
+    def test_read_neg(self, redis_client):
+        """Negative assertion."""
+        readex = redis_client.check_cache(self.METRIC_NAME)
+        assert readex != self.TEST_VAL - 1
+
+    def test_read_expired(self, redis_client):
+        """None right after ex time."""
+        tm.sleep(self.EXPIRATION_TIME_SEC)
+        readex = redis_client.check_cache(self.METRIC_NAME)
+        assert not readex
