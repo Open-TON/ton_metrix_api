e20abb13aa3240512e85698ba0e0fece2e01670e
Kirill Melcin <hi@kirillmelcin.ru>
2023-06-12 01:42:26 +0300

    Added tonapi.io service

--- src/services/tonapi.py
+++ src/services/tonapi.py
@@ -1,37 +1,57 @@
 # -*- coding: utf-8 -*-
-from fastapi import FastAPI, HTTPException
 import requests
+import os
 
-app = FastAPI()
-
-
-def request_to_ton_api(url_request: str):
-    print(url_request)
-    response = requests.get(url_request)
-    data = response.json()
-
-    return data
+TONAPI_KEY = os.getenv("TONAPI_KEY") or None
+TONAPI_BASE_URL = "https://tonapi.io/v2"
 
 
-# get transactions by block:
-@app.get("/api/v1/blockchain/blocks/{block_id}/transactions")
-def read_item(block_id: str):
-    url_request = "https://tonapi.io/v2/blockchain/blocks/"+block_id+"/transactions"
-    return request_to_ton_api(url_request)
+class TonAPIClientError(Exception):
+    pass
 
 
-# get transactions by account:
-@app.get("/api/v1/blockchain/accounts/{account_id}/transactions")
-def read_item(account_id: str):
-    url_request = "https://tonapi.io/v2/blockchain/accounts/"+account_id+"/transactions"
-    return request_to_ton_api(url_request)
+class TonAPIClient:
+    def _request(self, address, params=None):
+        params = {} if params is None else params
 
-@app.get("/api/v1/")
-def welcome():
-    return "Open TON API v1"
+        # Making a request to tonapi.io
+        if TONAPI_KEY:
+            response = requests.get(TONAPI_BASE_URL + address, params, headers={"Authorization": f"Bearer {TONAPI_KEY}"})
+        else:
+            response = requests.get(TONAPI_BASE_URL + address, params)
 
-@app.get("/api/v1/{request:path}")
-def read_item(request: str):
-    print(request)
-    url_request = "https://tonapi.io/v2/" + request
-    return request_to_ton_api(url_request)
+        # Checking the response
+        if response.status_code != requests.codes.ok:
+            raise TonAPIClientError(
+                f"Tonapi response with error code: {response.status_code}, error: {response.text}")
+        return response.json()
+
+    def tx_by_block(self, block_id):
+        """
+       Find transactions by block id.
+
+       :param str block_id: Block id e.g. "(-1,8000000000000000,4234234)"
+        """
+        request_url = f"/blockchain/blocks/{block_id}/transactions"
+        transactions = self._request(request_url)
+        return transactions
+
+    def tx_by_account(self, account_id):
+        """
+       Find transactions by account id.
+
+       :param str account_id: Account id in any format
+        """
+        request_url = f"/blockchain/accounts/{account_id}/transactions"
+        transactions = self._request(request_url)
+        return transactions
+
+    def raw_request(self, request_url, params):
+        """
+        Make any request to tonapi.io
+
+        :param request_url: Endpoint. e.g. "/rates?tokens=ton&currencies=rub"
+        :param params: Any parameters. e.g. {"tokens":"ton","currencies":"rub"}
+        """
+        response = self._request(request_url, params)
+        return response
